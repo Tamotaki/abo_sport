ed1c281a43f8e5082972d819b5753433eefeaf9f
Pierre TAJAN <tamotaki-pierro@outlook.com>
2026-03-03 14:07:38 +0100

    Push 2
    Amelio classe + Méthode toString

---
+++ prestation.java
@@ -0,0 +1,31 @@
+class Prestation {
+
+private String code;
+private String libelle;
+private double prix;
+
+//Constructeur de la classe prestation
+public Prestation (String code, String libelle, double prix) {
+    this.code = code;
+    this.libelle = libelle;
+    this.pri = prix;
+}
+
+//Getters
+public String getCode(){
+    return code;
+}
+publlic String getLibelle(){
+    return libelle;
+}
+public double getPrix(){
+    return prix;
+}
+
+//Méthode toString
+@Override
+public String toString(){
+    return code + libelle + prix + " € ";
+}
+
+}
